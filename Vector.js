868e1baeab3a392a105acc7248e6971aeacd750e
Diogo <86491085+Setserp13@users.noreply.github.com>
2024-10-19 00:31:08 -0300

    Update Vector.js

--- Vector.js
+++ Vector.js
@@ -55,3 +55,61 @@ class Vector extends Array
 
 	floor() { return new Vector(...this.map(Math.floor)); }
 }
+
+class Rect
+{
+	constructor(min, size)
+	{
+		this.min = min;
+		this.size = size;
+	}
+	
+	get center() { return this.min.add(this.extents) }
+
+	get extents() { return this.size.mul(0.5) }
+
+	get max() { return this.min.add(this.size) }
+
+	normalizePoint(point) { return point.sub(this.min).div(this.size); }
+
+	denormalizePoint(point) { return point.mul(this.size).add(this.min); }
+
+	normalizeVector(vector) { return point.div(this.size); }
+
+	denormalizeVector(vector) { return point.mul(this.size); }
+
+	normalizeRect(rect)
+	{
+		return new Rect(normalizePoint(rect.min), normalizeVector(rect.size))
+	}
+
+	denormalizeRect(rect)
+	{
+		return new Rect(denormalizePoint(rect.min), denormalizeVector(rect.size))
+	}
+}
+
+class Grid
+{
+	constructor(cellSize, cellGap = new Vector(0,0), offset = new Vector(0,0))
+	{
+		this.cellSize = cellSize;
+		this.cellGap = cellGap;
+		this.offset = offset;
+	}
+	
+	cellToPoint(value)
+	{
+		return this.offset.add(this.cellSize.add(this.cellGap).mul(value));
+	}
+
+	pointToCell(value)
+	{
+		return value.sub(this.offset).div(this.cellSize.add(this.cellGap)).floor();
+	}
+
+	cell(...coord)
+	{
+		return new Rect(this.cellToPoint(new Vector(...coord)), this.cellSize);
+	}
+}
